1a441feb796de7c760b3956e64aea239fbcc5c6d
Pavan Teja <143066166+pavanteja18@users.noreply.github.com>
2024-02-18 19:38:57 +0530

    Add files via upload

---
+++ Page/Reserve/list/menu/fooditem.js
@@ -0,0 +1,403 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 220,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/Ambur-Chicken-Biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.1,
+    price: 225,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/Chicken-Biryani-hyd.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 180,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/egg-biryani.jpeg?raw=true',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 3.8,
+    price: 190,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/goan-fish-biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.2,
+    price: 310,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/hyd-Mutton-Biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 220,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/kamrupi-biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.1,
+    price: 230,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/kashmiri.pulao.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 200,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/memonibiryani.png?raw=true',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 3.7,
+    price: 275,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/biryani/mughlai-biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.5,
+    price: 175,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Chicken_roast.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.2,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Chicken-Curry.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.7,
+    price: 160,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Chicken-do-Pyaza.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.0,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Chicken-Masala.jpeg?raw=true',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.1,
+    price: 270,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Handi-chicken.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 290,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chicken/Murgh-Musallam.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.6,
+    price: 150,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/Matar-Paneer.jpg?raw=true',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.0,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/palak-paneer.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.4,
+    price: 175,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/paneer-butter-masala.jpg?raw=true',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.5,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/Paneer-Do-Pyaza.jpg?raw=true',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.7,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/Paneer-Hyderabadi.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 170,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/paneer-lababdar.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.0,
+    price: 250,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/paneer/shahi-paneer.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 3.9,
+    price: 180,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/navratan-korma_-vegetable.png?raw=true',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 175,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/VEG-JALFREZI.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 150,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/vegetable-biryani.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.7,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/vegetable-curry.jpeg?raw=true',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 100,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/vegetable-kolhapuri.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 145,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/vegetable-masala.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.0,
+    price: 65,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/vegetable/vegetable-pakora.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/cabbage-momos-.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.7,
+    price: 225,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/ChickenManchurian.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.2,
+    price: 180,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/Chili-Chicken.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.0,
+    price: 160,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/chowmin.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.8,
+    price: 100,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/spring-rolls.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.2,
+    price: 150,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/szechuan-chicken.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 3.9,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/chinese/veg-fried-rice.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.5,
+    price: 125,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/Butter-Masala-Dosa.png?raw=true',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.2,
+    price: 60,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/idli-with-rice-flour.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.0,
+    price: 75,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/masala-dosa.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.7,
+    price: 70,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/mysore-bonda.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.9,
+    price: 50,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/onion-uttapam.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.1,
+    price: 60,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/plain-dosa.jpeg?raw=true',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 45,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/Rava-Uttapam.jpg?raw=true',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.6,
+    price: 60,
+    img: 'https://github.com/Ahmad528/shoppingsite/blob/main/images/south%20indian/sambhar-vada.jpg?raw=true',
+    quantity: 1
+},
+]
+export {foodItem};
